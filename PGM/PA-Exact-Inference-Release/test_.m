5d90ab0c403970e7afef12b172297d606989785a
litao <believeyour@qq.com>
2018-01-29 10:36:24 +0800

    wip

--- PGM/PA-Exact-Inference-Release/test_.m
+++ PGM/PA-Exact-Inference-Release/test_.m
@@ -3,7 +3,7 @@ load PA4Sample.mat
 ComputeInitialPotentials(InitPotential.INPUT);
 GetNextCliques(GetNextC.INPUT1,GetNextC.INPUT2);
 CliqueTreeCalibrate(SumProdCalibrate.INPUT);
-ComputeExactMarginalsBP( ExactMarginal.INPUT);
+a = ComputeExactMarginalsBP( ExactMarginal.INPUT);
 
 FactorMaxMarginalization(FactorMax.INPUT1,FactorMax.INPUT2);
 ComputeExactMarginalsBP(MaxMarginals.INPUT);
