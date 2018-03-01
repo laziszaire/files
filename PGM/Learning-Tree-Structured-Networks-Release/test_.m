fbcbf26738afb81a33c9eca9737c8a6f3069a048
litao <believeyour@qq.com>
2018-03-02 02:09:40 +0800

    wip

--- PGM/Learning-Tree-Structured-Networks-Release/test_.m
+++ PGM/Learning-Tree-Structured-Networks-Release/test_.m
@@ -5,3 +5,11 @@ dataset = exampleINPUT.t3a3;
  D1 = squeeze(dataset(1,:,:));
  loglikelihood = ComputeLogLikelihood(P, G, dataset);
  loglikelihood-exampleOUTPUT.t3
+ 
+ %%
+ clear
+load PA8SampleCases.mat
+dataset = exampleINPUT.t4a1;
+G = exampleINPUT.t4a2;
+labels = exampleINPUT.t4a3;
+[P,loglikelihood] = LearnCPDsGivenGraph(dataset, G, labels);
